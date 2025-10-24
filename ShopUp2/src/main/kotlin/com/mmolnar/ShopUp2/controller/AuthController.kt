d1887440b2f295f975811fbc4ddcca700667a70c
Molnár Márton <127486642+mmolnar01@users.noreply.github.com>
2025-10-24 11:19:25 +0200

    AuthController

---
+++ ShopUp2/src/main/kotlin/com/mmolnar/ShopUp2/controller/AuthController.kt
@@ -0,0 +1,19 @@
+package com.mmolnar.ShopUp2.controller
+
+import com.mmolnar.ShopUp2.service.UserService
+import lombok.RequiredArgsConstructor
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import jakarta.validation.Valid
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+class AuthController {
+    private final lateinit var userService: UserService
+    private final lateinit var passwordEncoder : PasswordEncoder
+
+    
+}
